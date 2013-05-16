0849124b1ab515c67f3af15f8e6a1467b6d7591e
danielCarlosCE <danielcarlossmd@gmail.com>
2013-05-16 20:24:37 -0300

    First

--- Runnign4Life/src/com/example/runnign4life/dao/CorridaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.runnign4life.dao;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class CorridaDAO {
-	public static List<String> corridas = new ArrayList<String>();
-}
